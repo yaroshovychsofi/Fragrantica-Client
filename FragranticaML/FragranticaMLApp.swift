7f01fe0aca34d703f73ed001c8c56ce8e5ab9dcc
yaroshovychsofi <sofia.yaroshovych@gmail.com>
2026-03-27 02:43:45 +0200

    Initial Commit

---
+++ FragranticaML/FragranticaMLApp.swift
@@ -0,0 +1,17 @@
+//
+//  FragranticaMLApp.swift
+//  FragranticaML
+//
+//  Created by Софія Ярошович on 27.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct FragranticaMLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
